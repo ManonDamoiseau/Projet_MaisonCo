1ac69f0e8680017bc47b293edd48e0ea02dd881e
ManonDamoiseau <m.damoiseau05@hotmail.com>
2025-08-28 15:33:47 +0200

    Paramétrage options JWT

--- ProjetMaisonCo_backend/Program.cs
+++ ProjetMaisonCo_backend/Program.cs
@@ -1,3 +1,10 @@
+
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ProjetMaisonCo_backend.Entities.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("Main")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer( o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            builder.Configuration.GetSection("TokenInfo").GetSection("secret").Value!)),
+        ValidateIssuer = false,
+        ValidIssuer = builder.Configuration.GetSection("TokenInfo").GetSection("issuer").Value!,
+        ValidAudience = builder.Configuration.GetSection("TokenInfo").GetSection("audience").Value!,
+        ValidateLifetime = false
+    };
+});
 
 var app = builder.Build();
 
--- ProjetMaisonCo_backend/obj/Release/net8.0/ProjetMaisonCo_backend.AssemblyInfo.cs
+++ ProjetMaisonCo_backend/obj/Release/net8.0/ProjetMaisonCo_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetMaisonCo_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad72f2ffc39fc122750fd01c65ff9d68643431fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d39f212fef8b088a381637eb1f09d4442af7052e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetMaisonCo_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetMaisonCo_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
