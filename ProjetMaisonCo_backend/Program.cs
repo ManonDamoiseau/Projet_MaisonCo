5d19410a2fcbb45d810498033bfa3254ed761a3d
ManonDamoiseau <m.damoiseau05@hotmail.com>
2025-08-28 15:52:40 +0200

    Mise en place de politiques JWT (attention localhost à vérifier dans program.cs)

--- ProjetMaisonCo_backend/Program.cs
+++ ProjetMaisonCo_backend/Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ProjetMaisonCo_backend.Entities.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,6 +31,49 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 });
 
+//Mise en place des politiques
+builder.Services.AddAuthorization(o =>
+{
+    o.AddPolicy("Auth", p => p.RequireAuthenticatedUser());
+    o.AddPolicy("Admin", p => p.RequireRole("Admin"));
+});
+
+builder.Services.AddCors(o =>
+{
+    o.AddDefaultPolicy(p =>
+    { //adapter le lien localhost au projet!!
+        p.WithOrigins("http://localhost:4200", builder.Configuration.GetSection("TokenInfo").GetSection("audience").Value!);
+        p.AllowAnyHeader();
+        p.AllowAnyMethod();
+    });
+});
+
+// Pour passer du JWT avec SWAGGER
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyProject", Version = "v1.0.0" });
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+    c.AddSecurityDefinition("Bearer", securitySchema);
+    var securityRequirement = new OpenApiSecurityRequirement
+                 {
+                     { securitySchema, new[] { "Bearer" } }
+                 };
+    c.AddSecurityRequirement(securityRequirement);
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,7 +85,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
--- ProjetMaisonCo_backend/obj/Release/net8.0/ProjetMaisonCo_backend.AssemblyInfo.cs
+++ ProjetMaisonCo_backend/obj/Release/net8.0/ProjetMaisonCo_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetMaisonCo_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d39f212fef8b088a381637eb1f09d4442af7052e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac69f0e8680017bc47b293edd48e0ea02dd881e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetMaisonCo_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetMaisonCo_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
